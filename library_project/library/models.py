70d150cfc6df1ba35a2781564a3c5a810ca78a7a
Nikhil7709 <jadenik13@gmail.com>
2025-12-16 23:56:55 +0530

    feat | Write models

--- library_project/library/models.py
+++ library_project/library/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=128) 
+    email = models.EmailField(unique=True) 
+    address = models.CharField(
+        max_length=256, 
+        blank=True, 
+        null=True 
+    ) 
+    
+    def __str__(self): 
+        return f"{self.name} - {self.email}" 
+
+
+class Book(models.Model): 
+    title = models.CharField(max_length=256) 
+    published_date = models.DateField() 
+    author = models.ForeignKey(
+        Author, 
+        on_delete=models.CASCADE, 
+        related_name='books_author' 
+    ) 
+    
+    def __str__(self): 
+        return f"{self.title} - {self.author.name}"
+
