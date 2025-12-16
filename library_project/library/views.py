c1a8eac3cfbcfb8ebe18b5bec5ef64b504dcc984
Nikhil7709 <jadenik13@gmail.com>
2025-12-17 00:29:50 +0530

    feat | Created AuthorListView

--- library_project/library/views.py
+++ library_project/library/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from library.models import Author
 # Create your views here.
+
+class AuthorListView:
+    def get(self, request):
+        authors = Author.objects.all()
+        return render(request, 'library/author_list.html', {'authors': authors})
+
